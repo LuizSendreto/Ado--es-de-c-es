92f02472c2e92132614263fe8f8079a50e150b07
LuizSendreto <luiz_s_dos-santos@estudante.sesisenai.org.br>
2026-04-07 18:53:54 -0300

    up - arrumei a estilização do site

---
+++ script.js
@@ -0,0 +1,24 @@
+const form = document.getElementById("formAdocao");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const telefone = document.getElementById("telefone").value.trim();
+
+    
+    const telefoneNumeros = telefone.replace(/\D/g, "");
+
+    
+    if (telefoneNumeros.length < 8) {
+        alert("Telefone inválido! Deve ter pelo menos 8 dígitos.");
+        return;
+    }
+
+    
+    if (telefoneNumeros.length < 10 || telefoneNumeros.length > 11) {
+        alert("Telefone deve ter DDD + número (10 ou 11 dígitos).");
+        return;
+    }
+
+    alert("Formulário enviado com sucesso!");
+});
