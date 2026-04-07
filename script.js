6428deed43731fcdb5c6c6cdb6f5460e79d59f2e
LuizSendreto <luiz_s_dos-santos@estudante.sesisenai.org.br>
2026-04-07 20:53:00 -0300

    up - adicionei e alterei nas paginas dos cachorros

--- script.js
+++ script.js
@@ -14,9 +14,43 @@ form.addEventListener("submit", function (e) {
         return;
     }
 
-    
-    if (telefoneNumeros.length < 10 || telefoneNumeros.length > 11) {
-        alert("Telefone deve ter DDD + número (10 ou 11 dígitos).");
+    if (!quintal) {
+        alert("Informe se possui quintal!");
+        return;
+    }
+
+    // Pet
+    if (!pet) {
+        alert("Informe se já teve pet!");
+        return;
+    }
+
+    if (pet.value === "nao") {
+        alert("A ONG poderá acompanhar sua adaptação com o pet.");
+    }
+
+    if (horas === "" || isNaN(horas)) {
+        alert("Informe as horas corretamente!");
+        return;
+    }
+
+    if (horas > 8) {
+        alert("O animal ficará muito tempo sozinho!");
+    }
+
+    if (motivo.length < 10 || motivo === "quero" || motivo === "porque sim") {
+        alert("Motivo inválido!");
+        return;
+    }
+
+    if (!termo) {
+        alert("Você deve aceitar o termo!");
+        return;
+    }
+
+    // Coerência
+    if (moradia === "apartamento" && quintal.value === "sim") {
+        alert("Apartamento não pode ter quintal!");
         return;
     }
 
